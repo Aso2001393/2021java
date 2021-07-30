19db3aa5e6115d90a3cdff7033cba2fc3319892b
Aso2001393 <82856348+Aso2001393@users.noreply.github.com>
2021-07-30 18:01:47 +0900

    Add files via upload

---
+++ MainS.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class MainS{
+    public static void main(String[] args) {
+        System.out.println("*********");
+        System.out.println("戦艦ゲーム");
+        System.out.println("*********");
+    }
+}
